caad20b18429d6f2a2dadc64d014486f3423e37e
alejandrodavidmalavet <alejandrodavidmalavet@gmail.com>
2021-02-13 16:38:21 -0600

    parts 1-3 done

--- streamer.py
+++ streamer.py
@@ -7,6 +7,8 @@ import struct
 import concurrent.futures
 import time
 
+import hashlib
+
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -18,10 +20,10 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
 
-        self.send_seq = 0x00000001
+        self.send_seq = 0x00000000
         self.send_buffer = dict()
 
-        self.recv_seq = 0x00000001
+        self.recv_seq = 0x00000000
         self.recv_buffer = dict()
 
         self.closed = False
@@ -30,65 +32,74 @@ class Streamer:
         self.executor.submit(self.listener)
 
         self.acks = set()
+        self.fin = False
 
         
 
 
     def send(self, data_bytes: bytes) -> None:
         for data in self.partition_data(data_bytes):
-            self.send_buffer[self.send_seq] = self.build_packet(self.send_seq,0,data)
+            self.send_buffer[self.send_seq] = self.build_packet(self.send_seq,False,False,data)
             self.socket.sendto(self.send_buffer[self.send_seq], (self.dst_ip, self.dst_port))
-            #if self.send_seq not in self.acks: self.acks[self.send_seq] = False
             time.sleep(0.25)
-            if self.send_seq in self.acks:
-                self.send_seq += 1
-            else :
-                print("Resending Packet #" + str(self.send_seq))
+            while self.send_seq not in self.acks:
                 self.send(data_bytes)
+                time.sleep(0.25)
+            self.send_seq += 1
 
     def send_ack(self,seq):
-        self.socket.sendto(self.build_packet(seq,True,bytes()), (self.dst_ip, self.dst_port))
+        #print("Sending Acknowledgement for Packet #" + str(seq))
+        self.socket.sendto(self.build_packet(seq,True,False,bytes()), (self.dst_ip, self.dst_port))
+
+    def send_fin(self):
+        self.socket.sendto(self.build_packet(self.send_seq,False,True,bytes()), (self.dst_ip, self.dst_port))
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
         while self.recv_seq not in self.recv_buffer:
             continue
-        self.send_ack(self.recv_seq)
         self.recv_seq += 1
         return self.recv_buffer.pop(self.recv_seq - 1)
 
     def close(self) -> None:
+    
+
+        while self.send_seq not in self.acks:
+            self.send_fin()
+            time.sleep(0.1)
+        #print("FIN recieved")
+        time.sleep(2)
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         self.closed = True
         self.socket.stoprecv()
-        self.executor.shutdown()
 
     def listener(self):
         while not self.closed: # a later hint will explain self.closed
             try:
+                packet = self.socket.recvfrom()[0]
+                if packet:
+                    seq, ack, fin, data = self.deconstruct_packet(packet)
 
-                seq, ack, data = self.deconstruct_packet(self.socket.recvfrom()[0])
-
-                if ack:
-                    self.acks.add(seq)
-                else:
-                    self.recv_buffer[seq] = data
+                    if ack:
+                        self.acks.add(seq)
+                    else:
+                        self.recv_buffer[seq] = data
+                        self.send_ack(seq)
                 
             except Exception as e:
                 print("listener died!")
                 print(e)
-                self.close()
 
-    def build_packet(self, seq, ack, data):
-        f = 'I?' + str(len(data)) + 's'
-        return struct.pack(f, seq, ack, data)
+    def build_packet(self, seq, ack, fin, data):
+        f = 'I??' + str(len(data)) + 's'
+        return struct.pack(f, seq, ack, fin, data)
 
     def deconstruct_packet(self, packet):
-        f = 'I?' + str(len(packet)-5) + 's'
+        f = 'I??' + str(len(packet)-6) + 's'
         return struct.unpack(f, packet)
 
     def partition_data(self,data):
-        return (data[0 + i : 1467 + i] for i in range(0, len(data), 1467))
+        return (data[0 + i : 1466 + i] for i in range(0, len(data), 1466))
 
